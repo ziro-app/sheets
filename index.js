4d2d31dfafc84a9eee86d40fb8a6d4f8192e3c2b
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-12-01 22:15:14 -0800

    fix auth

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-const {GoogleAuth} = require('google-auth-library');
- 
-/**
- * Instead of specifying the type of client you'd like to use (JWT, OAuth2, etc)
- * this library will automatically choose the right client based on the environment.
- */
+const { google } = require('googleapis')
+
 async function main() {
-  const auth = new GoogleAuth({
-    scopes: 'https://www.googleapis.com/auth/cloud-platform'
-  });
-  const client = await auth.getClient();
-  const projectId = await auth.getProjectId();
-  const url = `https://dns.googleapis.com/dns/v1/projects/${projectId}`;
-  const res = await client.request({ url });
-  console.log(res.data);
+	const keyFile = './credentials.json'
+	const scopes = 'https://www.googleapis.com/auth/spreadsheets'
+	const client = await google.auth.getClient({ keyFile, scopes })
+	const ss = google.sheets('v4').spreadsheets
+	const res = await ss.get({
+		auth: client,
+		spreadsheetId:''
+	})
+	console.log(res)
 }
- 
-main().catch(console.error);
+
+main().catch(console.error)
