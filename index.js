756b4255372182be95c032509709f2ea475351c7
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-12-05 21:05:13 -0800

    receive resource and method of sheets API as parameters

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const sheets = require('googleapis').google.sheets
 const sheet = sheets('v4').spreadsheets
 const authorize = require('./authorize')
 
-const main = async () => {
+const request = async (resource, method) => {
 	try {
 		const auth = await authorize()
 		try {
-			const { data } = await sheet.values.get({
+			const { data } = await sheet[resource][method]({
 				auth,
-				spreadsheetId: '',
-				range: 'Recebimentos!A1:B1'
+				spreadsheetId: '1ypZ0N2ii1kfr1-oM1moVputhIS4I3SCrg2d6S9Q_ATY',
+				range: 'Test!A1:C1'
 			})
 			console.log(data)
 		} catch (error) {
@@ -21,4 +21,4 @@ const main = async () => {
 	}
 }
 
-main()
+request('values', 'get')
