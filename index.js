afbd06c9c7f97b479cbaa0019539ba8f98c39e78
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-12-05 21:15:25 -0800

    add more function parameters

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const sheets = require('googleapis').google.sheets
 const sheet = sheets('v4').spreadsheets
 const authorize = require('./authorize')
 
-const request = async (resource, method) => {
+const request = async (resource, method, spreadsheetId, range, others) => {
 	try {
 		const auth = await authorize()
 		try {
 			const { data } = await sheet[resource][method]({
 				auth,
-				spreadsheetId: '1ypZ0N2ii1kfr1-oM1moVputhIS4I3SCrg2d6S9Q_ATY',
-				range: 'Test!A1:C1'
+				spreadsheetId,
+				range,
+				...others
 			})
 			console.log(data)
 		} catch (error) {
@@ -21,4 +22,4 @@ const request = async (resource, method) => {
 	}
 }
 
-request('values', 'get')
+request('values', 'get', '1ypZ0N2ii1kfr1-oM1moVputhIS4I3SCrg2d6S9Q_ATY', 'Test!A1:C1')
