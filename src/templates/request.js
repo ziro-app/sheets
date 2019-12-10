0393d4a96306b3ca911af029d4b75d2960f441f9
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-12-10 15:58:56 -0800

    improve error message back to client

--- src/templates/request.js
+++ src/templates/request.js
@@ -16,14 +16,6 @@ const request = async (headers, body, queryStringParameters) => {
 			body: JSON.stringify('content-type header must be application/json and body must be a raw json')
 		}
 	const { apiResource, apiMethod, spreadsheetId, range, resource, otherParams } = body
-	/* if any required param is missing from body request, then return error message */
-	if (!apiResource) return { statusCode: 400, body: JSON.stringify('apiResource is required') }
-	if (!apiMethod) return { statusCode: 400, body: JSON.stringify('apiMethod is required') }
-	if (!spreadsheetId) return { statusCode: 400, body: JSON.stringify('spreadsheetId is required') }
-	if (!range) return { statusCode: 400, body: JSON.stringify('range is required') }
-	if (!resource) return { statusCode: 400, body: JSON.stringify('resource is required') }
-	if (!otherParams) return { statusCode: 400, body: JSON.stringify('otherParams is required') }
-	/* if all required params are present, then authorize and call google sheets api */
 	try {
 		const auth = await authorize()
 		const { data } = await sheet[apiResource][apiMethod]({
@@ -44,10 +36,9 @@ const request = async (headers, body, queryStringParameters) => {
 			const { code, message } = error.response.data.error
 			throw { statusCode: code, body: message }
 		} else {
-			console.log('Unexpected error:', error)
 			return {
 				statusCode: 500,
-				body: JSON.stringify('Internal error. Check logs', null, 4)
+				body: JSON.stringify(`Internal error. ${error}`, null, 4)
 			}
 		}
 	}
