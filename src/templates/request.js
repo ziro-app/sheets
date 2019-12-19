17618e738a69db3d4678edd896e57eac22a815df
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-12-19 14:08:50 -0800

    add more validations

--- src/templates/request.js
+++ src/templates/request.js
@@ -1,21 +1,25 @@
 const sheets = require('googleapis').google.sheets
 const sheet = sheets('v4').spreadsheets
 const authorize = require('../auth/authorize')
-const { isApiResourceValid, validResources } = require('../validations/index')
+const {
+	areQueryParamsValid,
+	areHeadersValid,
+	isApiResourceValid,
+	validResources
+} = require('../validations/index')
 
 const request = async (headers, body, queryStringParameters) => {
-	if (Object.getOwnPropertyNames(queryStringParameters).length > 0)
+	const { apiResource, apiMethod, ...otherParams } = body
+	if (!areQueryParamsValid(queryStringParameters))
 		return {
 			statusCode: 400,
-			body: JSON.stringify('query string parameters are not allowed')
+			body: JSON.stringify('usage of query string parameters is not allowed')
 		}
-	if (headers['content-type'] !== 'application/json')
+	if (!areHeadersValid(headers))
 		return {
 			statusCode: 400,
 			body: JSON.stringify('content-type header must be application/json and body must be a raw json')
 		}
-	const { apiResource, apiMethod, ...otherParams } = body
-	console.log(!isApiResourceValid(apiResource))
 	if (!isApiResourceValid(apiResource))
 		return {
 			statusCode: 400,
@@ -23,15 +27,12 @@ const request = async (headers, body, queryStringParameters) => {
 		}
 	try {
 		const auth = await authorize()
-		console.log(auth)
 		const { data } = await sheet[apiResource][apiMethod]({ auth, ...otherParams })
 		return {
 			statusCode: 200,
 			body: JSON.stringify(data, null, 4)
 		}
 	} catch (error) {
-		if (error.apiResourceError)
-			throw { statusCode: 400, body: error.message }
 		if (error.authError)
 			throw { statusCode: 500, body: JSON.stringify(error.message, null, 4) }
 		if (error.response && error.response.data && error.response.data.error) {
--- src/validations/index.js
+++ src/validations/index.js
@@ -1,4 +1,6 @@
 const validResources = ['developerMetadata','sheets','values']
 
+exports.areQueryParamsValid = queryParams => Object.getOwnPropertyNames(queryParams).length > 0
+exports.areHeadersValid = headers => headers['content-type'] !== 'application/json'
 exports.isApiResourceValid = apiResource => validResources.some(validResource => validResource === apiResource)
 exports.validResources = validResources
