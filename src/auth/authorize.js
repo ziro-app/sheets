6b5a725a0e901513d1ad4c370dfee86defc3ef65
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-12-20 15:31:52 -0800

    debug

--- src/auth/authorize.js
+++ src/auth/authorize.js
@@ -3,6 +3,7 @@ const scopes = 'https://www.googleapis.com/auth/spreadsheets'
 
 const authorize = async () => {
 	try {
+		console.log(process.env.NETLIFY)
 		return auth.getClient({ scopes, credentials: {
 			'private_key': process.env.NETLIFY ? JSON.parse(process.env.PRIVATE_KEY) : process.env.PRIVATE_KEY,
 			'client_email': process.env.CLIENT_EMAIL
