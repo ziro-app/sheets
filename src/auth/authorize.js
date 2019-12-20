b0eb14a3e3f9c963c8ea92df38f0a3126349cf79
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-12-20 15:01:34 -0800

    debug

--- src/auth/authorize.js
+++ src/auth/authorize.js
@@ -4,7 +4,7 @@ const scopes = 'https://www.googleapis.com/auth/spreadsheets'
 const authorize = async () => {
 	try {
 		return auth.getClient({ scopes, credentials: {
-			'private_key': process.env.ORIGINAL_KEY,
+			'private_key': JSON.parse(process.env.ORIGINAL_KEY),
 			// 'private_key': Buffer.from(process.env.PRIVATE_KEY, 'base64').toString('ascii'),
 			'client_email': process.env.CLIENT_EMAIL
 		}})
