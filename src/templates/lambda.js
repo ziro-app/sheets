43c6a115e399aa9b86c185aff5c8075a8d3d7c2c
Wermeson Rocha <wermesonrocha2@gmail.com>
2021-06-30 19:47:38 -0300

    fix: Minor fix

--- src/templates/lambda.js
+++ src/templates/lambda.js
@@ -1,18 +1,15 @@
 require('dotenv').config()
 const middy = require('@middy/core')
 const { preflight } = require('@ziro/middleware')
-const { allowedOrigin } = require('@ziro/middleware')
 const { auth } = require('@ziro/middleware')
 const checkIfPostRequest = require('../utils/checkIfPostRequest')
 const jsonBodyParser = require('@middy/http-json-body-parser')
 const { errorHandler } = require('@ziro/middleware')
 const { cors } = require('@ziro/middleware')
-const allowed = ['http://localhost:7070', 'http://localhost:8080', 'http://localhost:9090']
 
 const lambda = handler =>
 	middy(handler)
 		.use(preflight)
-		// .use(allowedOrigin(allowed))
 		.use(auth)
 		.use(checkIfPostRequest)
 		.use(jsonBodyParser())
